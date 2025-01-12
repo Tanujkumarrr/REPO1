6abb2d442949717f384747a99a28d778c2509550
MY PC <MY PC@192.168.1.4>
2025-01-12 21:42:12 +0530

    tl

--- AIrthmatic/src/com/nt/git/Sum.java
+++ AIrthmatic/src/com/nt/git/Sum.java
@@ -4,7 +4,7 @@ public class Sum {
 	public static void sum(int x, int y) {
 		int result = x +y ;
 		System.out.println("hi Tanuj kumar");
-		System.out.println("TL");
+		System.out.println("TL good afternoon");
 		System.out.println("result "+result);
 		
 	}
