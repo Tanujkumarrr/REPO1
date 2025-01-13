7b0ff6636f8e5642704b3dd80063e22fb77c0b82
MY PC <MY PC@192.168.1.4>
2025-01-13 18:25:16 +0530

    tl

--- Sum/src/com/nt/git/SumOfTwoNum.java
+++ Sum/src/com/nt/git/SumOfTwoNum.java
@@ -3,7 +3,7 @@ package com.nt.git;
 public class SumOfTwoNum {
 	public static void sum(int x, int y) {
 		int result = x + y ;
-		System.out.println("HII TL TANUJ KUMAR");
+		System.out.println("HII TL TANUJ KUMAR ");
 		
 	}
 }
