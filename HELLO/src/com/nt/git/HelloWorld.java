561ff4ffeee032bd43a0f01c38d286fb11913b18
MY PC <MY PC@192.168.1.4>
2025-01-13 18:50:54 +0530

    dev

--- HELLO/src/com/nt/git/HelloWorld.java
+++ HELLO/src/com/nt/git/HelloWorld.java
@@ -2,7 +2,7 @@ package com.nt.git;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hii TL");
+		System.out.println("hii dev TAnuj");
 		System.out.println("hii TAnuj");
 	
 	}
