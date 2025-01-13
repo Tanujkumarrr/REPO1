aad1378255a1c550672960a5abddc02808e6cdca
MY PC <MY PC@192.168.1.4>
2025-01-13 18:43:33 +0530

    tl

--- AIrthmatic/src/com/nt/git/Airthmatic.java
+++
@@ -1,9 +0,0 @@
-package com.nt.git;
-
-public class Airthmatic {
-
-	public static void main(String[] args) {
-	Sum.sum(100, 200);
-	
-	}
-}
--- Sum/src/com/nt/git/SumOfTwoNum.java
+++
@@ -1,9 +0,0 @@
-package com.nt.git;
-
-public class SumOfTwoNum {
-	public static void sum(int x, int y) {
-		int result = x + y ;
-		System.out.println("HII TL TANUJ KUMAR ");
-		
-	}
-}
